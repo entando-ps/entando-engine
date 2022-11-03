8af9d1f34d8651176adf7b0f609a68f5dfb7bc6f
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2022-11-03 11:08:13 +0100

    SMEMT-16: Update tenant config (to analyze better due Redis issue) # Conflicts: #	src/main/java/org/entando/entando/ent/services/tenant/cache/TenantManagerCacheWrapper.java

--- src/main/java/org/entando/entando/ent/services/tenant/cache/TenantManagerCacheWrapper.java
+++ src/main/java/org/entando/entando/ent/services/tenant/cache/TenantManagerCacheWrapper.java
@@ -22,6 +22,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 import org.entando.entando.ent.exception.EntException;
+import org.entando.entando.ent.exception.EntRuntimeException;
 import org.entando.entando.ent.services.tenant.TenantConfig;
 import org.entando.entando.ent.util.EntLogging.EntLogFactory;
 import org.entando.entando.ent.util.EntLogging.EntLogger;
